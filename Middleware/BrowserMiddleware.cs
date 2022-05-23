20e1fb30b16a1a86808a2d359e44fa9571ca8243
jakkir <jakkir@LAPTOP-2GNB6A2H>
2022-05-23 10:55:20 +0200

    Dodaj pliki projektów.

---
+++ Middleware/BrowserMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Middlewares;
+
+public class BrowserMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public BrowserMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var userAgent = context.Request.Headers["User-Agent"].ToString();
+        if (userAgent.Contains("MSIE") || userAgent.Contains("Trident") || userAgent.Contains("Edg"))
+        {
+            context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+            return;
+        }
+
+        // Call the next delegate/middleware in the pipeline.
+        await _next(context);
+    }
+}
+
+public static class RequestCultureMiddlewareExtensions
+{
+    public static IApplicationBuilder UseBrowserMiddleware(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<BrowserMiddleware>();
+    }
+}
